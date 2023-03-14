a903bd6d7509c9d1f800903da6316ec78acac126
Jeremie4 <126706125+Jeremie4@users.noreply.github.com>
2023-03-14 11:04:41 -0600

    Update task2.py

--- task2.py
+++ task2.py
@@ -1,12 +1,15 @@
 import turtle as t
 
+# Gets radius and location
 radius = int(input("Bullseye Radius: "))
 target_location = input("Target Location (X Y): ")
 
+# Gets seperate X and Y values 
 x, y = target_location.split(" ")
 x = int(x)
 y = int(y)
 
+# Draw first circle
 t.pu()
 t.goto(x, (radius * -4) + y)
 t.pd()
@@ -15,6 +18,7 @@ t.begin_fill()
 t.circle(radius * 4)
 t.end_fill()
 
+# Draw second circle
 t.pu()
 t.goto(x, (radius * -3) + y)
 t.pd()
@@ -23,6 +27,7 @@ t.begin_fill()
 t.circle(radius * 3)
 t.end_fill()
 
+# Draw third circle
 t.pu()
 t.goto(x, (radius * -2) + y)
 t.pd()
@@ -31,6 +36,7 @@ t.begin_fill()
 t.circle(radius * 2)
 t.end_fill()
 
+# Draw fourth circle
 t.pu()
 t.goto(x, (radius * -1) + y)
 t.pd()
@@ -39,5 +45,5 @@ t.begin_fill()
 t.circle(radius)
 t.end_fill()
 
-
+# Hold
 hold = input("Press ENTER to exit.")
